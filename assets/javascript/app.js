8de6bb0de0bbcb9fd3f7d2143b8fe80ac4639709
Ernesto <esamaniego53@gmail.com>
2018-11-12 06:54:25 +0000

    added app.js and styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,124 +1,132 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>firebase authentication</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+$(document).ready(() => {
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Black+And+White+Picture|Comfortaa|Raleway" rel="stylesheet">
- <link rel="stylesheet" href="../css/style.css">
-  </head>
-  <body>
-    
-    
-<!--navbar-->
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDhh7ltx2_E4iQViobKonFTjsg3ioLVW50",
+        authDomain: "signup-form-2d114.firebaseapp.com",
+        databaseURL: "https://signup-form-2d114.firebaseio.com",
+        projectId: "signup-form-2d114",
+        storageBucket: "",
+        messagingSenderId: "574957317259"
+    };
+    firebase.initializeApp(config);
 
-<nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><i class="fas fa-fire"></i> firebase to do's</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    var database = firebase.database();
+    var auth = firebase.auth();
+    const signUpForm = $("#sign-up-form");
+    const signUpEmail = $("#sign-up-email");
+    const signUpPassword = $("#sign-up-password");
+    const logOutBtn = $("#logout");
+    const signInForm = $("#sign-in-form");
+    const signInEmail = $("#sign-in-email");
+    const signInPassword = $("#sign-in-password");
+    const title = $(".title");
+    const welcome = $(".welcome-message");
+    const toDoList = $("#to-do-list");
+    const addItemForm = $("#add-item-form");
+    const navBar = $("#navbar");
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <!--<input class="form-control mr-sm-2" type="search" placeholder="Search">-->
-      <button id="logout" class="btn btn-secondary">Logout</button>
-    </form>
-  </div>
-</nav>
+    // for signing up new user
+    signUpForm.on("submit", e => {
+        e.preventDefault();
+        const email = signUpEmail.val();
+        const pass = signUpPassword.val();
 
-<!--end of navbar-->
-    
-    
-    
-    <div class="container mx-auto">
-        <div class="jumbotron mt-3 col-sm-10 col-md-8 col-lg-7 mx-auto">
-    
-        <!-- Content here -->
-<!-- form -->
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch(e => alert(e.message));
+        $("#error-message").html("<h4>" + e.message + "</h4>")
+        //need to show this to the page when email and pass is already registered
 
-<!--new form-->
-<h2 class="text-center title"><i class="fas fa-lock"></i> Login to your account</h2>
-<hr/>
-<form id="sign-in-form">
-  <div class="form-group col-sm-6  mx-auto">
-    <label for="sign-in-email"><i class="fas fa-at"></i> Email address</label>
-    <input type="email" class="form-control" id="sign-in-email" aria-describedby="emailHelp" placeholder="Enter email" required>
+    });
+
+    //sign in existing user
+    signInForm.on("submit", e => {
+        e.preventDefault();
+        const signEmail = signInEmail.val();
+        const signInPass = signInPassword.val();
+
+        const promise = auth.signInWithEmailAndPassword(signEmail, signInPass);
+        promise.catch(e => alert(e.message));
+        $(".welcome-message").html("<h4>" + e.message + "</h4>")
+
+    });
+
+
+    logOutBtn.on("click", () => {
+
+        auth.signOut().then( () => {
+            // Sign-out successful.
+            console.log("Signed Out");
+        }).catch((error) => {
+            // An error happened.
+            console.log(error);
+            
+        });
+
+    });
+
+//for global access
+let uid = "";
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+
+        if (firebaseUser) {
+            console.log(firebaseUser);
+            //hide signup for here
+            navBar.show();
+            signUpForm.hide();
+            signInForm.hide();
+            title.hide();
+            welcome.show();
+            logOutBtn.show();
+            toDoList.show()
+            uid = firebaseUser.uid;
+            $(".welcome-message").html("<h4>" + "Welcome: " + firebaseUser.email + "</h4>")
+
+// Use the UID to get authorized data
+    database.ref('/to-do-list').orderByChild('uid').equalTo(uid).on("value", (snapshot) => {
+      $('#to-do-list-items').empty()
+      snapshot.forEach(data => {
+        var p = $('<p>')
+        p.text(data.val().item)
+        var deleteButton = $("<button class='btn btn-danger mr-3 fa fa-eraser'>")
+        // deleteButton.html('x')
+        deleteButton.addClass('delete-button')
+        deleteButton.attr('data', data.key)
+        p.prepend(deleteButton)
+        $('#to-do-list-items').append(p)
+      })
+    })
+
+        }
+        else {
+            console.log("Not Logged In");
+            //show signup or log in form here
+            signUpForm.show()
+            signInForm.show()
+            logOutBtn.hide()
+            title.show()
+            welcome.hide()
+            toDoList.hide()
+            navBar.hide();
+        }
+    });
     
-  </div>
-  <div class="form-group col-sm-6 mx-auto">
-    <label for="sign-in-password"><i class="fas fa-key"></i> Password</label>
-    <input type="password" class="form-control" id="sign-in-password" placeholder="Password" required>
-  </div>
-  <!--<div class="form-group form-check col-sm-6 mx-auto">-->
-  <!--  <input type="checkbox" class="form-check-input" id="exampleCheck1">-->
-  <!--  <label class="form-check-label" for="exampleCheck1">Check me out</label>-->
-  <!--</div>-->
+    // delete event
+$(document).on('click', '.delete-button', function(){
+  key = $(this).attr('data');
+  database.ref('/to-do-list').child(key).remove();
   
-  <div class=" col-sm-6 mx-auto">
-  <button id="sign-in-button" type="submit" class="btn btn-primary col-sm-12 mx-auto">Submit</button>
-  </div>
-  <div class=" col-sm-6 mx-auto mt-3 text-center">Don't have an Account?<a href="../../index.html"> Sign Up</a></div>
-  <!--<div class=" col-sm-6 mx-auto">Already have an Account?<a href="signIn.html"> Login here</a></div>-->
-  
-</form>
-<!--end of new form-->
+});
 
-<div class="text-center mx-auto">
-<div class="welcome-message text-center mx-auto animated zoomOutUp delay-2s"></div>
+// add event
+$('#add-item-form').on('submit', (e) => {
+  e.preventDefault()
+  item = $('#item').val()
+  database.ref('/to-do-list').push({
+    item: item,
+    uid: uid
+  });
+  addItemForm[0].reset();
+});
 
-
-
-<!--<div class="col-auto">-->
-<!--            <button id="logout" class="btn btn-secondary mb-2 text-center mx-auto">Logout</button>-->
-<!--        </div>-->
-        </div>
-        
-        
-        <!-- Authorization -->
-        <div class="container">
-    <div id="to-do-list" class="dashed-container">
-      
-
-      <form id="add-item-form">
-        <h3>To Do's</h3>
-        <input type="text" id="item" class="form-control col-sm-6" placeholder="to do item" required>
-        <button type="submit" class="btn btn-dark mt-3">Add item</button>
-      </form>
-
-      <div class="mt-2" id="to-do-list-items"></div>
-
-    </div>
-    </div>
-    <hr/>
-        
-        
-</div>
-<!--end of jumbotron-->
-</div>
-
-    
-    
-    <script src="https://www.gstatic.com/firebasejs/5.5.8/firebase.js"></script>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-    <script src="../javascript/app.js"></script>
-  </body>
-</html>
+});
